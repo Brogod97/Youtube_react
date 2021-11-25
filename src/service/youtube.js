03a51b6d761943b52733599a09ec04cc748774ac
Hyeongshin <evergreen1286@gmail.com>
2021-11-25 19:10:46 +0900

    back to the list when searching

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -16,15 +16,13 @@ class Youtube {
     return result_1.items;
   }
 
-  search(query) {
-    return fetch(
+  async search(query) {
+    const response = await fetch(
       `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}}&type=video&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      );
+    );
+    const result_1 = await response.json();
+    return result_1.items.map((item) => ({ ...item, id: item.id.videoId }));
   }
 }
 
